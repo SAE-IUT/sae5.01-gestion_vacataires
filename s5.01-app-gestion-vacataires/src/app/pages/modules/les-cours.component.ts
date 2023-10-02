5874c72f515789fe6cd038f4f33ca2d654101f13
EnzoMancini <enzomancini930@gmail.com>
2023-10-02 16:34:16 +0200

    Ajout creation de modules

--- s5.01-app-gestion-vacataires/src/app/pages/modules/les-cours.component.ts
+++ s5.01-app-gestion-vacataires/src/app/pages/modules/les-cours.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { ModulesService } from 'src/app/services/modules.service';
 
 @Component({
   selector: 'app-les-cours',
@@ -6,5 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./les-cours.component.css']
 })
 export class LesCoursComponent {
+
+  public modules: any[] = []
+
+  form = {
+    name : "",
+    name_reduit: "",
+    color_hexa: "",
+    departement: [""],
+    matiere: ""
+  }
+
+  constructor(private modulesService: ModulesService) {}
+
+  ngOnInit() {
+    this.modulesService.getModule().subscribe((data: any) => {
+      this.modules = data;
+    });
+  }
+
+  onSubmit(name: string, name_reduit: string, color_hexa: string, departement: string[], matiere: string) {
+    this.modulesService.addModule(name, name_reduit, color_hexa, departement, matiere).subscribe({
+      next: (response) => {
+        window.location.reload()
+      },
+      error: (error) => {
+        // Gestion des erreurs
+        console.error(error);
+      },
+      complete: () => {
+      }
+    });
+  }
+
+  addModule(name: string, name_reduit: string, color_hexa: string, departement: string[], matiere: string) {
+
+    this.modulesService.addModule(name, name_reduit, color_hexa, departement, matiere).subscribe({
+      next: (response) => {
+        window.location.reload()
+      },
+      error: (error) => {
+        // Gestion des erreurs
+        console.error(error);
+      },
+      complete: () => {
+      }
+    });
+
+  }
+
+  hello(value: string) {
+    console.log(typeof(value));
+
+  }
 
 }
