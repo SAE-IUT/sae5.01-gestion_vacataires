b9625cc1c9470898ffcdc4800e1920a8056e1913
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-10-03 12:05:38 +0200

    Modification Connexion

--- s5.01-app-gestion-vacataires/src/app/pages/connection/connection.component.ts
+++ s5.01-app-gestion-vacataires/src/app/pages/connection/connection.component.ts
@@ -7,13 +7,16 @@ import { NgForm }   from '@angular/forms';
     <form (ngSubmit)="onSubmit(loginForm)">
       <input type="text" name="username" ngModel required>
       <input type="password" name="password" ngModel required>
-      <button type="submit" [disabled]="!loginForm.valid">Login</button>
+      <button type="submit" [disabled]="!loginForm?.valid">Login</button>
     </form>
   `
 })
-export class LoginComponent {
-  onSubmit(form: NgForm) {
-    console.log(form.value);
+export class ConnectionComponent {
+
+  loginForm: NgForm | undefined;
+
+  onSubmit(form: NgForm | undefined) {
+    console.log(form?.value);
     // Do login here
   }
 }
