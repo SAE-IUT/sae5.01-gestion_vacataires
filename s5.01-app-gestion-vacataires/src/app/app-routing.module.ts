1f56f64456e1f065d2b06ac6b1b80be6aff53732
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-10-03 12:06:00 +0200

    changes for connexion

--- s5.01-app-gestion-vacataires/src/app/app-routing.module.ts
+++ s5.01-app-gestion-vacataires/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { VacatairesComponent } from './pages/vacataires/vacataires.component';
 import { AccueilComponent } from './pages/accueil/accueil.component';
 import { LesCoursComponent } from './pages/modules/les-cours.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ConnectionComponent } from './pages/connection/connection.component';
 
 const routes: Routes = [
   { path : '', component: VacatairesComponent },
   { path : 'accueil', component: AccueilComponent },
+  { path : 'connexion', component: ConnectionComponent },
   { path: 'vacataires', component:VacatairesComponent },
   { path: 'les-cours', component:LesCoursComponent },
   { path: '404', component:NotFoundComponent },
