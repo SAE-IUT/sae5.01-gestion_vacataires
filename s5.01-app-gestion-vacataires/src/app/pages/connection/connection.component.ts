a82ee88565c8ff5436ce9b63e352edb9b4fa6576
IUT BLAGNAC <noreply@univ-tlse2.fr>
2023-10-03 11:17:52 +0200

    Create form

--- s5.01-app-gestion-vacataires/src/app/pages/connection/connection.component.ts
+++ s5.01-app-gestion-vacataires/src/app/pages/connection/connection.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgForm }   from '@angular/forms';
 
 @Component({
-  selector: 'app-connection',
-  templateUrl: './connection.component.html',
-  styleUrls: ['./connection.component.css']
+  selector: 'app-login',
+  template: `
+    <form (ngSubmit)="onSubmit(loginForm)">
+      <input type="text" name="username" ngModel required>
+      <input type="password" name="password" ngModel required>
+      <button type="submit" [disabled]="!loginForm.valid">Login</button>
+    </form>
+  `
 })
-export class ConnectionComponent {
-
+export class LoginComponent {
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    // Do login here
+  }
 }
